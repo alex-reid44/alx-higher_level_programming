e42c5e477dbea042b550f130d142bbf7f761a43a
alex-reid44 <kipchirchiralex36@gmail.com>
2023-09-11 14:21:23 -0700

    all

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,2 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    num = len(sys.argv)
-    sum = 0
-    if num > 1:
-        for i in range(1, num):
-            sum += int(sys.argv[i])
-        print("{}".format(sum))
-    else:
-        print("{}".format(sum))
+#!/usr/bin/node
+console.log(process.argv[2] || 'No argument');
